fa605900f95e076281f690425e9c7bcee5d34b1d
Duncan Hamill <Duncan Hamill>
2016-08-21 21:40:52 +0100

    Initial commit with creation of solution and basic kernel

---
+++ AugustusDev/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AugustusDev
+{
+    public class Kernel
+    {
+        public readonly string VersionNumber = "0.0.1";
+
+        static void Main(string[] args)
+        {
+            Kernel kern = new Kernel();
+            kern.BeforeRun();
+            kern.Run();
+        }
+
+        public void BeforeRun()
+        {
+            Console.WriteLine("Augustus\nVersion {0}", VersionNumber);
+        }
+
+        public void Run()
+        {
+            Console.ReadLine();
+        }
+    }
+}
